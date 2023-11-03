54534ac619d09de940560d78ecdb8ac3fb0e745d
R.Gowtham <149375848+gowthamravi1993@users.noreply.github.com>
2023-11-03 11:40:58 +0530

    Update Test5.java
    change the output

--- src/org/sample/Test5.java
+++ src/org/sample/Test5.java
@@ -5,6 +5,7 @@ public class Test5 {
 		System.out.println(1);
 		System.out.println(2);
 		System.out.println(3);
+		System.out.println(3a);
 		System.out.println(4);
 		System.out.println(5);
 		System.out.println(6);
